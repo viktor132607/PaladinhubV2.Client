c64c81e4791764cac8c0ea66ad8fe692e807336c
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-09-09 14:48:44 +0300

    Use layout guide header for Protection Talents

--- src/legacy-pages/guides/protection/Talents.tsx
+++ src/legacy-pages/guides/protection/Talents.tsx
@@ -1,19 +1,15 @@
 "use client";
 
-import { GuidePageHeader } from "@/components/migration/MigratedView";
 import SectionTalentTrees from "@/components/talent-trees/SectionTalentTrees";
 
 export default function Talents() {
   return (
-    <>
-      <GuidePageHeader />
-      <div className="outer-wrapper">
-        <div className="page-container">
-          <div className="main-wrapper">
-            <SectionTalentTrees section="protection" />
-          </div>
+    <div className="outer-wrapper">
+      <div className="page-container">
+        <div className="main-wrapper">
+          <SectionTalentTrees section="protection" />
         </div>
       </div>
-    </>
+    </div>
   );
 }
